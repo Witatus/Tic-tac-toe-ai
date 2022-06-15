1cc5f914fc90f4e5ed1c4bc6e671d59da5caa4af
mateusz_rozanowski <m.rozanowski@stuent.uj.edu.pl>
2022-06-15 14:26:12 +0200

    smiga wszystko

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def stop_game(board):
 
 class Board:
     def __init__(self):
-        self.current_board = np.array([[5, 5, 0],
+        self.current_board = np.array([[0, 0, 0],
                                        [0, 0, 0],
                                        [0, 0, 0]])
         self.current_player = 1
@@ -22,7 +22,7 @@ class Board:
         else:
             if self.current_player == 1:
                 self.current_board[x][y] = 1
-                self.current_player = 5
+                self.current_player = 2
             else:
                 self.current_board[x][y] = 5
                 self.current_player = 1
@@ -66,10 +66,22 @@ class Board:
 
     def ai_move(self):
 
-        if self.check_win():
+        if self.check_win(): #done
             return
-        if self.check_block():
+        if self.check_block(): #done tutaj sie cos jebie
             return
+        if self.check_fork(): #done
+            return
+        if self.check_player_fork(): #done
+            return 
+        if self.check_center(): #done
+            return 
+        if self.opposite_corner(): #done
+            return
+        if self.empty_corner(): #done
+            return 
+        if self.empty_side(): #done
+            return 
 
         return
 
@@ -102,6 +114,31 @@ class Board:
             self.move(ai_ind[0], ai_ind[1])
             return True
 
+    def check_win_without_move(self):
+        wins = 0
+        for i in range(3):
+            if sum(self.current_board[i]) == 10:
+                wins = wins + 1
+
+        for i in range(3):
+            if sum(self.current_board[:, i]) == 10:
+                wins = wins + 1
+
+        if (self.current_board[0][0] + self.current_board[1][1]) == 10 and self.current_board[2][2] == 0:
+            wins = wins + 1
+        if (self.current_board[0][0] + self.current_board[2][2]) == 10 and self.current_board[1][1] == 0:
+            wins = wins + 1
+        if (self.current_board[1][1] + self.current_board[2][2]) == 10 and self.current_board[0][0] == 0:
+            wins = wins + 1
+        if (self.current_board[2][0] + self.current_board[1][1]) == 10 and self.current_board[0][2] == 0:
+            wins = wins + 1
+        if (self.current_board[2][0] + self.current_board[0][2]) == 10 and self.current_board[1][1] == 0:
+            wins = wins + 1
+        if (self.current_board[1][1] + self.current_board[0][2]) == 10 and self.current_board[2][0] == 0:
+            wins = wins + 1
+
+        return wins
+
     def check_block(self):
         # check if player can win in next move
         ai_ind = []
@@ -111,7 +148,7 @@ class Board:
 
         for i in range(3):
             if sum(self.current_board[:, i]) == 2:
-                ai_ind = [list(self.current_board[i]).index(0), i]
+                ai_ind = [list(self.current_board[:, i]).index(0), i]
 
         if (self.current_board[0][0] + self.current_board[1][1]) == 2 and self.current_board[2][2] == 0:
             ai_ind = [2, 2]
@@ -132,6 +169,108 @@ class Board:
             self.move(ai_ind[0], ai_ind[1])
             return True
 
+    def check_fork(self):
+        ai_ind = []
+        for i in range(3):
+            for j in range(3):
+                if self.current_board[i][j] == 0:
+
+                    self.current_board[i][j] = 5    #robie ruch
+
+                    # sprawdzam czy po ruchu mam wiecej niz 1 opcje na wygreanie
+                    wins = self.check_win_without_move()
+
+                    if wins >= 2:
+                        ai_ind = [i, j]
+
+                    self.current_board[i][j] = 0
+
+        if len(ai_ind) == 0:
+            return False
+        else:
+            self.move(ai_ind[0], ai_ind[1])
+            return True
+
+    def check_player_fork(self):
+        ai_ind = []
+        for i in range(3):
+            for j in range(3):
+                if self.current_board[i][j] == 0:
+
+                    self.current_board[i][j] = 1  # robie ruch
+
+                    # sprawdzam czy po ruchu mam wiecej niz 1 opcje na wygreanie
+                    wins = self.check_win_without_move()
+
+                    if wins >= 2:
+                        ai_ind = [i, j]
+
+                    self.current_board[i][j] = 0
+
+        if len(ai_ind) == 0:
+            return False
+        else:
+            self.move(ai_ind[0], ai_ind[1])
+            return True
+
+    def check_center(self):
+        if self.current_board[1][1] == 0:
+            self.move(1, 1)
+            return True
+        else:
+            return False
+
+    def opposite_corner(self):
+        ai_ind = []
+        if self.current_board[0][0] == 1 and self.current_board[2][2] == 0:
+            ai_ind = [2, 2]
+        elif self.current_board[2][2] == 1 and self.current_board[0][0] == 0:
+            ai_ind = [0, 0]
+        elif self.current_board[0][2] == 1 and self.current_board[2][0] == 0:
+            ai_ind = [2, 0]
+        elif self.current_board[2][0] == 1 and self.current_board[0][2] == 0:
+            ai_ind = [0, 2]
+
+        if len(ai_ind) == 0:
+            return False
+        else:
+            self.move(ai_ind[0], ai_ind[1])
+            return True
+
+    def empty_corner(self):
+        ai_ind = []
+        if self.current_board[0][0] == 0:
+            ai_ind = [0, 0]
+        elif self.current_board[2][2] == 0:
+            ai_ind = [2, 2]
+        elif self.current_board[2][0] == 0:
+            ai_ind = [2, 0]
+        elif self.current_board[0][2] == 0:
+            ai_ind = [0, 2]
+
+        if len(ai_ind) == 0:
+            return False
+        else:
+            self.move(ai_ind[0], ai_ind[1])
+            return True
+
+    def empty_side(self):
+        ai_ind = []
+        if self.current_board[1][0] == 0:
+            ai_ind = [1, 0]
+        elif self.current_board[0][1] == 0:
+            ai_ind = [0, 1]
+        elif self.current_board[1][2] == 0:
+            ai_ind = [1, 2]
+        elif self.current_board[2][1] == 0:
+            ai_ind = [2, 1]
+
+        if len(ai_ind) == 0:
+            return False
+        else:
+            self.move(ai_ind[0], ai_ind[1])
+            return True
+
 
 if __name__ == '__main__':
     b = Board()
@@ -139,8 +278,8 @@ if __name__ == '__main__':
 
     while counter != 9:
         b.print_board()
-        # b.ai_move()
-        # b.print_board()
-        player_move = input()
-        b.move(int(player_move[0]), int(player_move[2]))
+        player_move = list(map(int, input().split()))
+        b.move(int(player_move[0]), int(player_move[1]))
+        b.print_board()
         b.ai_move()
+        counter = counter + 1
